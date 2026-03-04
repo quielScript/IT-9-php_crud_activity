6948e9146ea743615f94b3709d2a105c165819ea
quielScript <exequielarco23@gmail.com>
2026-03-04 22:00:37 +0800

    chore: fixed clunkly spacing caused by default formatter

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
   $servername = "localhost";
-  $username   = "root";
-  $password   = "";
-  $dbname     = "php_crud_activity";
+  $username = "root";
+  $password = "";
+  $dbname = "php_crud_activity";
 
   $conn = new mysqli($servername, $username, $password, $dbname);
 
